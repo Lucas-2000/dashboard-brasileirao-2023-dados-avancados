ee231cda38156bb4ec781995f5c3a7d95f9ac7a2
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-01-09 14:43:12 -0300

    [feat] dashboard finalizado

---
+++ 1_home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import webbrowser
+
+# if "data" not in st.session_state:
+#   df_data = pd.read_csv("datasets/regular_overall_season_serie_a.csv", index_col=0)
+
+#   # Gravando dataframe no cache da sessão
+#   st.session_state["data"] = df_data
+  
+st.write("# Dados avançados do campeonato brasileiro! ⚽")
+st.sidebar.markdown("Desenvolvido por [Lucas Marchesoni](https://www.linkedin.com/in/lucasmarchesoni/)")
+
+btn = st.button("Acesse os dados")
+if btn:
+  webbrowser.open_new_tab("https://github.com/Lucas-2000/api-sports-stats")
+  
+st.markdown(
+  """
+    O conjunto dados contém informações avançadas até a rodada 38 do campeonato brasileiro de 2023.
+    Possui uma ampla gama de métricas como xG, xGA, Saves%, Passes, etc. Possibilitando realizar uma análise
+    mais a fundo dentro de uma partida de futebol e todas suas nuances.
+    
+    Para poder explorar cada dado possui **24 bases** para poderem ser analisadas, extraídos insights, gerado gráficos
+    e poder exportar os resultados para poder compartilhar em outros meios.
+    
+    Todos os dados foram retirados do site [Football Reference](https://fbref.com/en/comps/24/Serie-A-Stats).
+  """
+)
---
+++ pages/2_graficos.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+st.title("Gere os gráficos")
+
+dataset_directory = "D://Python//brasileirao_dash//datasets"
+
+datasets = sorted(os.listdir(dataset_directory))
+
+dataset = st.sidebar.selectbox("Datasets", datasets)
+
+if dataset:
+    dataset_path = os.path.join(dataset_directory, dataset)
+    
+    if dataset.endswith('.csv'):
+        df = pd.read_csv(dataset_path)
+        
+        st.write("## Dados:")
+        st.write(df)
+        
+        filtro1 = st.sidebar.selectbox("Eixo X", df.columns, key="x")
+        filtro2 = st.sidebar.selectbox("Eixo Y", df.columns, key="y")
+        
+        tipo_grafico = st.sidebar.selectbox("Tipo Gráfico", ['Selecione o tipo de gráfico', 'Gráfico de Linhas', 'Gráfico de Barras', 'Gráfico de Dispersão', 'Histograma'])
+        
+        if tipo_grafico != 'Selecione o tipo de gráfico':
+            st.write("## Gráfico")
+            if tipo_grafico in ['Gráfico de Linhas', 'Gráfico de Barras']:
+                exibir_grafico = st.sidebar.checkbox("Exibir Gráfico")
+                if exibir_grafico:
+                    if tipo_grafico == 'Gráfico de Linhas':
+                        st.line_chart(df.set_index(filtro1)[filtro2])
+                    elif tipo_grafico == 'Gráfico de Barras':
+                        st.bar_chart(df.set_index(filtro1)[filtro2])
+            elif tipo_grafico == 'Gráfico de Dispersão':
+                st.write("## Gráfico de Dispersão")
+                fig, ax = plt.subplots()
+                ax.scatter(df[filtro1], df[filtro2])
+                for i, txt in enumerate(df['Squad']):
+                    ax.annotate(txt, (df[filtro1][i], df[filtro2][i]))
+                st.pyplot(fig)
+            elif tipo_grafico == 'Histograma':
+                st.write("## Histograma")
+                plt.hist(df[filtro1])
+                st.pyplot()
+        else:
+            st.info("Por favor, selecione um tipo de gráfico. Caso deseje utilizar o histograma, o Eixo X é utilizado como base para geração.")
+    else:
+        st.error("Este exemplo só suporta arquivos CSV.")
